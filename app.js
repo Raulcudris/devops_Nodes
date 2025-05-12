45bfc18fc4957b69211299bd1e22899a85a674c5
raulcudris <raulcudriz@hotmail.com>
2025-05-12 16:44:51 -0500

    Actualizacion

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.get('/', (req, res) =>{
-    res.send("Backend Node.js funcionando!")
+    res.send("Backend Node.js funcionando!!")
 });
 
 app.listen(PORT, ()=>{
